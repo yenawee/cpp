c865fe7321f391110df0c7adff314230996fc96d
yenawee <yaena0319@naver.com>
2022-11-30 00:55:54 +0900

    ex02 준비

---
+++ cpp00/ex02/Account.cpp
@@ -0,0 +1,64 @@
+#include "Account.hpp"
+
+static int	Account::getNbAccounts( void ){
+
+};
+
+static int	Account::getTotalAmount( void ){
+
+
+};
+
+static int	Account::getNbDeposits( void ){
+
+
+};
+
+static int	Account::getNbWithdrawals( void ){
+
+
+};
+
+static void	Account::displayAccountsInfos( void ){
+
+
+};
+
+Account::Account(void){
+
+}
+
+Account::Account( int initial_deposit ){
+
+
+};
+
+Account::~Account( void ){
+
+
+};
+
+void	makeDeposit( int deposit ){
+
+
+};
+
+bool	makeWithdrawal( int withdrawal ){
+
+
+};
+
+int		checkAmount( void ) const{
+
+
+};
+
+static void	_displayTimestamp( void ){
+
+
+};
+
+void	displayStatus( void ) const{
+
+
+};
