34fdbf8ec30b7545d49b169784bf934929e0d1e0
yenawee <yaena0319@naver.com>
2022-12-03 11:16:22 +0900

    ㅈㅏ리이동

---
+++ cpp02/ex00/Fixed.hpp
@@ -0,0 +1,20 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+    private :
+        int _n;
+        static const int _N_OF_BITS = 8;
+    
+    public :
+        Fixed();
+        ~Fixed();
+        Fixed(Fixed const & src);
+        Fixed & operator=(Fixed const & rhs);
+        int getRawBits( void ) const;
+        void setRawBits( int const raw );
+};
+
+#endif
---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,25 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+    private :
+        int _n;
+        static const int _N_OF_BITS = 8;
+    
+    public :
+        Fixed();
+        ~Fixed();
+        Fixed(Fixed const & src);
+        Fixed(int const n);
+        Fixed(float const n);
+        Fixed & operator=(Fixed const & rhs);
+        int getRawBits( void ) const;
+        void setRawBits( int const raw );
+        float toFloat(void) const;
+        int toInt(void) const;
+};
+
+#endif
