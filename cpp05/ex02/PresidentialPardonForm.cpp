bf41a5262b73f353f2125215cd11c60a07277fd9
yenawee <yaena0319@naver.com>
2022-12-16 20:55:54 +0900

    for daily commit

--- cpp05/ex02/PresidentialPardonForm.cpp
+++ cpp05/ex02/PresidentialPardonForm.cpp
@@ -1,25 +1,25 @@
 #include "PresidentialPardonForm.hpp"
 
-PresidentialPardonForm::PresidentialPardonForm(void) : 
-{ 
-    // std::cout << "PresidentialPardonForm created. Default." << std::endl; 
+PresidentialPardonForm::PresidentialPardonForm(void) :
+{
+    // std::cout << "PresidentialPardonForm created. Default." << std::endl;
 }
 
 PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm & src)
-{ 
-    std::cout << "Copy constructor called. " << std::endl; 
-     *this = src; 
+{
+    std::cout << "Copy constructor called. " << std::endl;
 }
 
 PresidentialPardonForm::~PresidentialPardonForm(void)
-{ 
-    std::cout << "PresidentialPardonForm destroyed." << std::endl; 
+{
+    std::cout << "PresidentialPardonForm destroyed." << std::endl;
 }
 
 PresidentialPardonForm & PresidentialPardonForm::operator=(const PresidentialPardonForm & rhs)
 {
     if (this != &rhs)
     {
+        target = rhs.target;
     }
     return *this;
 }
